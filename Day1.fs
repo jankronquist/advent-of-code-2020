185a032663ec51aa09a7fe4ea90bc9333af9d0be
Jan Kronquist <jan.kronquist@gmail.com>
2020-12-02 13:38:56 +0100

    Day1b

--- Day1.fs
+++ Day1.fs
@@ -4,13 +4,24 @@ open System
 
 let add x y = x + y
 
-let rec solveA = function
-    | [] -> -1
-    | (a :: rest) -> match List.filter (fun b -> a + b = 2020) rest with
-                        | [] -> solveA rest
-                        | (b :: r) -> (a * b)
+let rec solveA s numbers : int Option =
+    match numbers with
+    | [] -> None
+    | (a :: rest) -> match List.filter (fun b -> a + b = s) rest with
+                        | [] -> solveA s rest
+                        | (b :: r) -> Some (a * b)
+
+let rec solveB s numbers =
+    match numbers with
+    | [] -> None
+    | (a :: rest) -> match solveA (s - a) rest with
+                     | Some c -> Some (a * c)
+                     | _ -> solveB s rest
 
 let solve (rows : string list) =
     let parsed = List.map int rows
-    printfn "A=%i" (solveA parsed)
+    let a = solveA 2020 parsed
+    printfn "A=%i" (Option.get a)
+    let b = solveB 2020 parsed
+    printfn "B=%i" (Option.get b)
     0
